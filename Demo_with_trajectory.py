d5319738b56cce8952c60b092dac84af2c3ea7c7
Shakeeb Shaikh <eren.85yeager@gmail.com>
2026-03-22 11:02:49 +0530

    Add object detection and tracking loop

--- Demo_with_trajectory.py
+++ Demo_with_trajectory.py
@@ -19,3 +19,19 @@ h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 out = cv2.VideoWriter("RobustSort.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
 
 frame_num = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model.predict(source=frame, conf=0.5, iou=0.4, verbose=False)
+    detections = results[0].boxes.xyxy.cpu().numpy() if results[0].boxes else []
+
+    # Convert detections to list of tuples
+    rects = [tuple(map(int, box)) for box in detections]
+
+    # Update tracker
+    objects = tracker.update(rects, frame_number=frame_num)
+    trajectories = tracker.get_trajectories()
+  
